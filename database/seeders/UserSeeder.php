3205b7f8d8a47a5ede9cb5df8ac06a0af54cbae4
Nderi12 <nderikamau1212@gmail.com>
2023-08-16 11:51:05 +0300

    Adding user seeder to generate users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Jobs\SendEmailJob;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 150) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password'), // Default password
+            ]);
+
+            // Dispatch a send email job
+            dispatch(new SendEmailJob($faker->unique()->safeEmail));
+        }
+    }
+}
